80f72e1e414e650a90633f6e21cdd180d42306de
felipequecole <felipequecole@msn.com>
2017-07-02 14:58:32 -0300

    caso 22: ok

--- src/trabalho2/Calculador.java
+++ src/trabalho2/Calculador.java
@@ -15,7 +15,7 @@ public class Calculador extends LuazinhaBaseVisitor<String> {
     public String visitCorpodafuncao(LuazinhaParser.CorpodafuncaoContext ctx) {
         if(ctx.listapar1!=null)
         {
-          visitListapar(ctx.listapar1);   
+          visitListapar(ctx.listapar1);
         }
         if(ctx.bloco()!=null)
         {
@@ -28,25 +28,25 @@ public class Calculador extends LuazinhaBaseVisitor<String> {
     public String visitListapar(LuazinhaParser.ListaparContext ctx) {
          if(ctx.listadenomes_f!=null)
          {
-           TabelaDeSimbolos escopoAtual;                 
+           TabelaDeSimbolos escopoAtual;
            escopoAtual = escopos.topo();
-           escopoAtual.adicionarSimbolos(ctx.listadenomes_f.nomes, "parametro"); 
+           escopoAtual.adicionarSimbolos(ctx.listadenomes_f.nomes, "parametro");
          }
          return null;
     }
 
-   
-            
+
+
     @Override
-    public String visitPrograma(LuazinhaParser.ProgramaContext ctx) {        
+    public String visitPrograma(LuazinhaParser.ProgramaContext ctx) {
        escopos.empilhar(new TabelaDeSimbolos("global"));
-       visitTrecho(ctx.trecho()); 
+       visitTrecho(ctx.trecho());
        escopos.desempilhar();
        return null;
-        
+
     }
-    
-    
+
+
     @Override
     public String visitTrecho(LuazinhaParser.TrechoContext ctx) {
         String valor = "";
@@ -58,25 +58,25 @@ public class Calculador extends LuazinhaBaseVisitor<String> {
         {
             valor+=visitUltimocomando(ctx.ultimocomando());
         }
-        
+
         return valor;
     }
-    
+
     @Override
     public String visitComando(LuazinhaParser.ComandoContext ctx) {
-       
-       if(ctx.listavar1!= null) //listavar '=' listaexp 
+
+       if(ctx.listavar1!= null) //listavar '=' listaexp
        {
-           TabelaDeSimbolos escopoAtual;                 
+           TabelaDeSimbolos escopoAtual;
            escopoAtual = escopos.topo();
            for(String nome : ctx.listavar1.nomes)
            {
                if(escopos.existeSimbolo(nome) == false)
                 escopoAtual.adicionarSimbolo(nome, "variavel");
            }
-          
+
        }
-       
+
       if(ctx.chamadadefuncao()!=null) //chamadadefuncao
       {
           visitChamadadefuncao(ctx.chamadadefuncao());
@@ -110,44 +110,47 @@ public class Calculador extends LuazinhaBaseVisitor<String> {
          if(ctx.bloco_else!=null)
          {
              visitBloco(ctx.bloco_else);
-         }           
+         }
       }
-      
+
       if(ctx.exp_for1 != null) //'for' NOME '=' exp_for1=exp ',' exp_for2=exp (',' exp_for3=exp)? 'do' bloco_for=bloco 'end'
       {
-           
+
           TabelaDeSimbolos t2 = new TabelaDeSimbolos("for");
-          escopos.empilhar(t2);          
+          escopos.empilhar(t2);
           escopos.topo().adicionarSimbolo(ctx.NOME().getText(), "variavel");
           visitExp(ctx.exp_for1);
           visitExp(ctx.exp_for2);
           if(ctx.exp_for3!=null)
-            visitExp(ctx.exp_for1);  
+            visitExp(ctx.exp_for1);
           visitBloco(ctx.bloco_for);
           escopos.desempilhar();
       }
-      
+
       if(ctx.lista_nomes1 != null) //'for' lista_nomes1=listadenomes 'in' lista_exp2+=listaexp 'do' bloco_for2=bloco 'end'
       {
-          TabelaDeSimbolos t2 = new TabelaDeSimbolos("for");         
-          escopos.empilhar(t2);                 
+          TabelaDeSimbolos t2 = new TabelaDeSimbolos("for");
+          escopos.empilhar(t2);
           escopos.topo().adicionarSimbolos(ctx.lista_nomes1.nomes, "variavel");
           visitListaexp(ctx.listaexp);
-          visitBloco(ctx.bloco_for2);          
+          visitBloco(ctx.bloco_for2);
           escopos.desempilhar();
       }
-      if(ctx.cp1 != null) //'function' nomedafuncao corpodafuncao 
+      if(ctx.cp1 != null) //'function' nomedafuncao corpodafuncao
       {
-          TabelaDeSimbolos t2 = new TabelaDeSimbolos(visitNomedafuncao(ctx.nomedafuncao()));        
+          TabelaDeSimbolos t2 = new TabelaDeSimbolos(visitNomedafuncao(ctx.nomedafuncao()));
           escopos.empilhar(t2);
-          visitCorpodafuncao(ctx.cp1);        
+          if (ctx.nomedafuncao().metodo) {
+              escopos.topo().adicionarSimbolo("self", "parametro");
+          }
+          visitCorpodafuncao(ctx.cp1);
           escopos.desempilhar();
       }
-      if(ctx.cp2!=null) //'local' 'function' NOME cp2=corpodafuncao 
+      if(ctx.cp2!=null) //'local' 'function' NOME cp2=corpodafuncao
       {
-          TabelaDeSimbolos t2 = new TabelaDeSimbolos(ctx.NOME().getText());         
+          TabelaDeSimbolos t2 = new TabelaDeSimbolos(ctx.NOME().getText());
           escopos.empilhar(t2);
-          visitCorpodafuncao(ctx.cp2);        
+          visitCorpodafuncao(ctx.cp2);
           escopos.desempilhar();
       }
       if(ctx.lista_nomes2!=null) //'local' lista_nomes2=listadenomes ('=' lista_exp3=listaexp)?
@@ -157,13 +160,13 @@ public class Calculador extends LuazinhaBaseVisitor<String> {
           {
               visitListaexp(ctx.listaexp);
           }
-      }      
-      return null; 
+      }
+      return null;
     }
 
     @Override
     public String visitExpprefixo2(LuazinhaParser.Expprefixo2Context ctx) {
-        if(ctx.var1 !=null) 
+        if(ctx.var1 !=null)
         {
             visitVar(ctx.var1);
         }
@@ -177,7 +180,7 @@ public class Calculador extends LuazinhaBaseVisitor<String> {
     @Override
     public String visitVar(LuazinhaParser.VarContext ctx) {
         if(ctx.NOME() != null){
-           TabelaDeSimbolos escopoAtual;                 
+           TabelaDeSimbolos escopoAtual;
            escopoAtual = escopos.topo();
            if(escopos.existeSimbolo(ctx.nome) == false)
               escopoAtual.adicionarSimbolo(ctx.nome, "variavel");
@@ -188,7 +191,7 @@ public class Calculador extends LuazinhaBaseVisitor<String> {
     @Override
     public String visitListavar(LuazinhaParser.ListavarContext ctx) {
         if(ctx.nomes != null){
-           TabelaDeSimbolos escopoAtual;                 
+           TabelaDeSimbolos escopoAtual;
            escopoAtual = escopos.topo();
            for(String nome : ctx.nomes)
            {
@@ -200,36 +203,41 @@ public class Calculador extends LuazinhaBaseVisitor<String> {
     }
 
     @Override
+    public String visitChamadadefuncao(LuazinhaParser.ChamadadefuncaoContext ctx) {
+        return super.visitChamadadefuncao(ctx);
+    }
+
+    @Override
     public String visitNomedafuncao(LuazinhaParser.NomedafuncaoContext ctx) {
         return ctx.nome;
     }
-    
+
      @Override
     public String visitBloco(LuazinhaParser.BlocoContext ctx) {
-        return visitTrecho(ctx.trecho()); 
+        return visitTrecho(ctx.trecho());
     }
-    
-    
+
+
     @Override
-    public String visitCampo(LuazinhaParser.CampoContext ctx) {  
+    public String visitCampo(LuazinhaParser.CampoContext ctx) {
         return null;
     }
-    
+
       @Override
     public String visitSeparadordecampos(LuazinhaParser.SeparadordecamposContext ctx) {
         return ctx.getText();
     }
-    
+
      @Override
     public String visitOpbin(LuazinhaParser.OpbinContext ctx) {
        return ctx.getText();
-    }  
-    
+    }
+
     @Override
     public String visitOpunaria(LuazinhaParser.OpunariaContext ctx) {
-       return ctx.getText();    
-    }   
-    
+       return ctx.getText();
+    }
+
     @Override
     public String visitExp(LuazinhaParser.ExpContext ctx) {
        if(ctx.exp2 != null)
@@ -247,8 +255,8 @@ public class Calculador extends LuazinhaBaseVisitor<String> {
         }
         return null;
     }
-    
-    
-    
-    
+
+
+
+
 }
